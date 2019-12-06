49fa6c1c9628ff670992cad2c2ff0b8ab80ceae3
Gustavo Rosa <gth.rosa@uol.com.br>
2019-12-06 18:30:53 -0300

    Adding a loop in the plotting graphic file.

--- plot_graphics.py
+++ plot_graphics.py
@@ -3,26 +3,31 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Defining an input file
-input_file = 'lenet_256.pkl'
+# Defining input files
+input_file = ['lenet_256.pkl', 'lenet_256.pkl']
 
-# Opening input file
-with open(input_file, 'rb') as f:
-    # Loading pickle object
-    metrics = pickle.load(f)
+# Defining input models
+input_model = ['$L_{256}$', '$L_{576}$']
 
-# Calculates the mean and standard deviation
-mean_loss = np.mean(metrics['train_loss'], axis=0)
-std_loss = np.std(metrics['train_loss'], axis=0)
+# Iterate over all possible models
+for i in input_file:
+    # Opening input file
+    with open(i, 'rb') as f:
+        # Loading pickle object
+        metrics = pickle.load(f)
 
-# Plots the mean loss
-plt.plot(mean_loss)
+    # Calculates the mean and standard deviation
+    mean_loss = np.mean(metrics['train_loss'], axis=0)
+    std_loss = np.std(metrics['train_loss'], axis=0)
 
-# Fills the mean with standard deviation
-plt.fill_between(range(300), mean_loss - std_loss, mean_loss + std_loss, alpha=0.35)
+    # Plots the mean loss
+    plt.plot(mean_loss)
+
+    # Fills the mean with standard deviation
+    plt.fill_between(range(300), mean_loss - std_loss, mean_loss + std_loss, alpha=0.35)
 
 # Plots the legend
-plt.legend(['$L_{64}$'], loc='upper left')
+plt.legend(input_model, loc='upper left')
 
 # Plots the labels
 plt.title('Training convergence over all architectures')
